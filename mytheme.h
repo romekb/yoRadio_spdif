6d3e89aa81777a68b9b44e828b2ece8ddc6e06d1
romekb <romek_b@o2.pl>
2025-09-15 19:39:24 +0200

    Initial commit

---
+++ mytheme.h
@@ -0,0 +1,46 @@
+// File created with YoRadio Theme Editor created by AndrĂĄs Daradics
+// File last modified: 8/25/2024 5:20:03 PM
+// GitHub: https://github.com/andrasdaradici/YoRadio-Theme-Editor
+// Itch.io: https://andrasdaradici.itch.io/yoradio-theme-editor
+#ifndef _my_theme_h
+#define _my_theme_h
+
+#define ENABLE_THEME
+#ifdef  ENABLE_THEME
+
+#define COLOR_BACKGROUND     0,  0,  0
+#define COLOR_STATION_NAME     255,  255,  255
+#define COLOR_STATION_BG    0,  0,  249
+#define COLOR_STATION_FILL    0,  0,  249
+#define COLOR_SNG_TITLE_1    255,  255,  0
+#define COLOR_SNG_TITLE_2    255,  0,  255
+#define COLOR_WEATHER    0,  255,  0
+#define COLOR_VU_MAX    255,  0,  0
+#define COLOR_VU_MIN    0,  0,  255
+#define COLOR_CLOCK    255,  255,  255
+#define COLOR_CLOCK_BG    0,  0,  0
+#define COLOR_SECONDS    255,  255,  0
+#define COLOR_DAY_OF_W    255,  0,  202
+#define COLOR_DATE    108,  119,  255
+#define COLOR_BUFFER    157,  0,  79
+#define COLOR_IP    0,  255,  0
+#define COLOR_VOLUME_VALUE    255,  0,  0
+#define COLOR_RSSI    255,  255,  0
+#define COLOR_VOLBAR_OUT    255,  0,  0
+#define COLOR_VOLBAR_IN    255,  255,  0
+#define COLOR_DIGITS    100, 100, 255
+#define COLOR_DIVIDER    255,  255,  255
+#define COLOR_BITRATE    0,  255,  255
+#define COLOR_HEAP    255, 168, 162
+#define COLOR_PL_CURRENT    0, 0, 0
+#define COLOR_PL_CURRENT_BG    91, 118, 255
+#define COLOR_PL_CURRENT_FILL    91, 118, 255
+#define COLOR_PLAYLIST_0    255, 255, 255
+#define COLOR_PLAYLIST_1    255, 255, 255
+#define COLOR_PLAYLIST_2    255, 255, 255
+#define COLOR_PLAYLIST_3    255, 255, 255
+#define COLOR_PLAYLIST_4    255, 255, 255
+
+
+#endif
+#endif
